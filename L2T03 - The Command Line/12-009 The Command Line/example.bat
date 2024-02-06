6d95772dce254f8205cd9607df18bc98e2daa33b
negin-mgdm <negin.mgdm@gmail.com>
2024-02-06 17:21:40 +0000

    Updating files by deleting the original course materials

--- L2T03 - The Command Line/12-009 The Command Line/example.bat
+++
@@ -1,7 +0,0 @@
-:: This is a comment in the Command Line. Comments need to be placed at the beginning of a new line, you cannot place a comment after a command.
-
-mkdir hello_world
-cd hello_world
-mkdir full_stack
-dir
-cmd.exe
